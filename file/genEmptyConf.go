9b020221292d14a9c16372439a4cff036e1eeb35
dextercai <caiwenzhe2021@gmail.com>
2024-09-30 11:49:50 +0800

    update.

--- file/genEmptyConf.go
+++ file/genEmptyConf.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"encoding/json"
 	"gopkg.in/yaml.v3"
 	"os"
 )
@@ -9,6 +10,17 @@ import (
 func GenEmptyYamlFile[T comparable](destFilePath string) error {
 	cnf := new(T)
 	data, err := yaml.Marshal(&cnf)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(destFilePath, data, 0644)
+	return err
+}
+
+// GenEmptyJsonFile 用于生成空的json配置文件
+func GenEmptyJsonFile[T comparable](destFilePath string) error {
+	cnf := new(T)
+	data, err := json.Marshal(&cnf)
 	if err != nil {
 		return err
 	}
