fcc1478f371afd00685093ded0ebc6c3ab8aa48f
dextercai <caiwenzhe2021@gmail.com>
2024-10-21 20:50:02 +0800

    .

--- file/genEmptyConf.go
+++ file/genEmptyConf.go
@@ -7,7 +7,7 @@ import (
 )
 
 // GenEmptyYamlFile 用于生成空的Yaml配置文件
-func GenEmptyYamlFile[T comparable](destFilePath string) error {
+func GenEmptyYamlFile[T any](destFilePath string) error {
 	cnf := new(T)
 	data, err := yaml.Marshal(&cnf)
 	if err != nil {
@@ -18,7 +18,7 @@ func GenEmptyYamlFile[T comparable](destFilePath string) error {
 }
 
 // GenEmptyJsonFile 用于生成空的json配置文件
-func GenEmptyJsonFile[T comparable](destFilePath string) error {
+func GenEmptyJsonFile[T any](destFilePath string) error {
 	cnf := new(T)
 	data, err := json.Marshal(&cnf)
 	if err != nil {
